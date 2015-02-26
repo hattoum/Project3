d353122d14a4446c54028d010e7fa03ec87c021d
hattoum <hattoum96@gmail.com>
2015-02-26 16:16:14 +0200

    Completes product UI

--- public/client.js
+++ public/client.js
@@ -1,8 +1,14 @@
-var socket = io.connect("http://localhost:1337");	
+var socket = io.connect("http://localhost:1337");
+var optionsArray = ["Option 1","Option 2","Option 3","Option 4"];
 $(document).ready(function(){
+	var options;
+	optionsArray.forEach(function(entry){
+			options += "<option>"+entry+"</option>";
+	})
 	var resource = $("nav").find("li").first().next();
 	var resourceOn = false;
 	var productOn = false;
+	var rotate = 0;
 	resource.on("resource",function(){
 		$.ajax("./resource.html",{
 			success: function(res){
@@ -37,8 +43,26 @@ $(document).ready(function(){
 			}
 		});
 	});
+	$(".inner").on("click",".addResource",function(){
+		var resourceEntry = $("<div class='resourceEntryDiv'><select class='resourceEntry'>"+options+"</select><input type='text'><div class='removeResource'><less>-</less></div></div>");
+		$(resourceEntry).insertBefore(this).velocity("slideDown",400);
+		rotate += 90;
+		$(this).velocity({rotateZ: rotate+"deg"},400);
+	});
+	$(".inner").on("click",".removeResource",function(){
+		var thisOne = $(this);
+		console.log($(thisOne).parent().find("select").val());
+		$(this).velocity({rotateZ: "90deg",background: "#ff0000"},200)
+		setTimeout(function(){
+			$(thisOne).parent().velocity("slideUp",200)},200);
+		setTimeout(function(){$(thisOne).parent().remove()},401);
+	})
 	$(".inner").on("submit","form",function(e){
 		e.preventDefault();
-		console.log($(this).find("input").val());
+		if($(this).attr("id") ==="resourceForm"){
+			socket.emit("resourceReq");
+		}else if($(this).attr("id") ==="productForm"){
+			socket.emit("productReq");
+		}
 	})
 })	
--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var server = http.createServer(app);
-
-app.use(express.static(__dirname+"/public"));
 var io = require("socket.io")(server);
+app.use(express.static(__dirname+"/public"));
+io.on("connection",function(client){
+	client.on("productReq",function(data){
+		console.log("Hello, product");
+	});
+	client.on("resourceReq",function(data){
+		console.log("Hello, resource");
+	})
+})
 server.listen(1337,"127.0.0.1",function(){console.log("listening on 127.0.0.1:1337")});
